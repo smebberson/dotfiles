69ab62b85c86e628808c8d64574201d77db372bb
Scott Mebberson <scott@scottmebberson.com>
2018-10-04 17:27:08 +0930

    Added iTerm2 and sudolikeaboss.

--- install.sh
+++ install.sh
@@ -1,4 +1,24 @@
 #!/usr/bin/env bash
 
-brew tap caskroom/fonts
-brew cask install font-fira-code
+# Fira code
+if ! brew cask list -1 | grep -q "^$(echo 'font-fira-code' | cut -d' ' -f1)\$"; then
+
+    brew tap caskroom/fonts
+    brew cask install font-fira-code
+
+fi
+
+# iTerm2
+if ! brew cask list -1 | grep -q "^$(echo 'iterm2' | cut -d' ' -f1)\$"; then
+
+    brew cask install iterm2
+
+fi
+
+# sudolikeaboss (for iTerm)
+if ! brew list -1 | grep -q "^$(echo 'sudolikeaboss' | cut -d' ' -f1)\$"; then
+
+    brew tap ravenac95/sudolikeaboss
+    brew install sudolikeaboss
+
+fi
